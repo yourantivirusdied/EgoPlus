05a7b3237816fa765c50a8a51140424c3c52efa4
Vivienne Hawthorne <154943025+aishiteruyovivi@users.noreply.github.com>
2024-07-09 23:11:21 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is it that you wish to do?");
+            string unimportantAnswer = Console.ReadLine();
+            Console.WriteLine("Hmm, I suggest that you...");
+            Random rnd = new Random();
+            int answer = rnd.Next(1, 2);
+            if (answer == 1)
+            {
+                Console.WriteLine("don't do that.");
+            } else
+            {
+                Console.WriteLine("go ahead and do that!");
+            }
+        }
+    }
+}
